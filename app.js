8dc777a168174c99f1b249b7da82abb4fc8241b4
Rob <rdaly1490@gmail.com>
2015-07-15 17:05:20 -0500

    changing app.js

--- app.js
+++ app.js
@@ -18,15 +18,12 @@ var AchievementFunctions = require('./routes/AchievementFunctions');
 var mongoose = require('mongoose');
 var fs = require("fs");
 var config = require('./config/config');
-var uriUtil = require('mongodb-uri');
 // var facebook = require("./stormpath/facebook");
 // var emailConfig=require('./stormpath/email');
-var mongodbUri = 'mongodb://heroku_h565qv7q:iordaql93r9j0vu0ullb328jkv@ds047672.mongolab.com:47672/heroku_h565qv7q';
-var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 
 var stormpath = require('express-stormpath');
 
-mongoose.connect(mongooseUri, options);
+mongoose.connect('mongodb://heroku_h565qv7q:iordaql93r9j0vu0ullb328jkv@ds047672.mongolab.com:47672/heroku_h565qv7q');
 
 var app = express();
 
